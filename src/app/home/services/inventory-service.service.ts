2333ffb318359ec4aa49852619e4f06c389d7844
ampGular <ampgular@gmail.com>
2019-07-18 00:07:08 +0200

    okdk

--- src/app/home/services/inventory-service.service.ts
+++ src/app/home/services/inventory-service.service.ts
@@ -18,36 +18,28 @@ export class InventoryService {
     public http: HttpClient,
   ) {
     this.apiEndPoint = `${this.scheme}://${this.host}/${this.path}`;
-    this.username = 'admin0';
-    this.password = 'suoersecret';
+    this.username = 'admin';
+    this.password = 'supersecret';
   }
 
  getPluginDefinition(): Observable<any>  {
 
-
-
     const url = this.apiEndPoint + '/plugin/definition';
-
-    // const body = JSON.parse(JSON.stringify(promo));
-    // url changed from this.introUrl
 
     const body = {};
     const httpOptions = {
       headers: new HttpHeaders({
         'Content-Type': 'application/json',
-        Authorization: 'Basic ' + new Buffer(this.username + ':' + this.password).toString('base64')
+        Authorization: 'Basic ' +  btoa(this.username + ':' + this.password)
 
       })
     };
 
     return this.http.post(url, body, httpOptions);
-    // .map(this.extractData)
-    // .catch(this.handleError);
+
   }
 
   getProductSearch(request: any): Observable<any>  {
-
-
 
     const url = this.apiEndPoint + '/product/search';
 
@@ -56,26 +48,23 @@ export class InventoryService {
     const httpOptions = {
       headers: new HttpHeaders({
         'Content-Type': 'application/json',
-         Authorization: 'Basic ' + new Buffer(this.username + ':' + this.password).toString('base64')
+        Authorization: 'Basic ' +  btoa(this.username + ':' + this.password)
       })
     };
     return this.http.post(url, body, httpOptions);
   }
 
+
   getProductById(request: any): Observable<any>  {
-
-
 
     const url = this.apiEndPoint + '/product/getById';
 
     const body = JSON.parse(JSON.stringify(request));
 
-    console.log(body);
-
     const httpOptions = {
       headers: new HttpHeaders({
         'Content-Type': 'application/json',
-         Authorization: 'Basic ' + new Buffer(this.username + ':' + this.password).toString('base64')
+        Authorization: 'Basic ' +  btoa(this.username + ':' + this.password)
       })
     };
     return this.http.post(url, body, httpOptions);
